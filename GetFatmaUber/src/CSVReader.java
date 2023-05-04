024d30b462ed452bfe0671820315d649f5ff8f32
nirme <nirme@10.100.102.105>
2023-05-04 10:00:50 +0300

    fix read files

--- GetFatmaUber/src/CSVReader.java
+++ GetFatmaUber/src/CSVReader.java
@@ -2,28 +2,87 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class CSVReader {
+	
+	static String[] customerData;
+	static String[] serviceData;
+	static ArrayList<Integer> customersIDList = new ArrayList<>();
+	static ArrayList<String> customersNameList = new ArrayList<>();
+	static ArrayList<Integer> customersAgeList = new ArrayList<>();
+	static ArrayList<String> customersGenderList = new ArrayList<>();
+	static ArrayList<Integer> serviceCustomersIDList = new ArrayList<>();
+	static ArrayList<String> serviceTypeList = new ArrayList<>();
+	static ArrayList<String> serviceAreaList = new ArrayList<>();
+	static ArrayList<Integer> distanceList = new ArrayList<>();
+
 
 	public static void main(String[] args) {
-		
-		String customersPath = "Customers.txt";
-//		String serviceRequestsPath = "Service_Requests.txt";
-		
+
+		String customersPath = "C:\\Users\\nirme\\git\\GetFatmaUber\\GetFatmaUber\\src\\Customers.txt.csv";
+		String servicePath = "C:\\Users\\nirme\\git\\GetFatmaUber\\GetFatmaUber\\src\\Service_Requests.txt.csv";
+
 		BufferedReader inFile=null;
 		try
 		{
-		  	FileReader fr = new FileReader (customersPath);
-		inFile = new BufferedReader (fr);
+			// Read data from the first file
+			FileReader fr = new FileReader(customersPath);
+			inFile = new BufferedReader(fr);
+			// read and ignore the first row
+			inFile.readLine();
+
+			String line;
+			int lineNumber = 0; // add a line number counter
+			while ((line = inFile.readLine()) != null) {
+				if (lineNumber == 1) { // if it's the first row, skip it
+					lineNumber++;
+					continue;
+				}
+				customerData = line.split("'");
+				int customerID = Integer.parseInt(customerData[1]);
+				String name = customerData[3];
+				int age = Integer.parseInt(customerData[5]);
+				String gender = customerData[7];
+				customersIDList.add(customerID);
+				customersNameList.add(name);
+				customersAgeList.add(age);
+				customersGenderList.add(gender);
+				lineNumber++;
+			}
+
+			// Read data from the second file
+			fr = new FileReader(servicePath);
+			inFile = new BufferedReader(fr);
+			// read and ignore the first row
+			inFile.readLine();
+
+			lineNumber = 0;
+			while ((line = inFile.readLine()) != null) {
+				if (lineNumber == 1) { // if it's the first row, skip it
+					lineNumber++;
+					continue;
+				}
+				serviceData = line.split("'");
+				int serviceCustomerID = Integer.parseInt(serviceData[1]);
+				String serviceType = serviceData[3];
+				String serviceArea = serviceData[5];
+				int distance = Integer.parseInt(serviceData[7]);
+				serviceCustomersIDList.add(serviceCustomerID);
+				serviceTypeList.add(serviceType);
+				serviceAreaList.add(serviceArea);
+				distanceList.add(distance);
+				lineNumber++;
+			}
 		}
 
 		catch (FileNotFoundException exception)
 		{
-		 System.out.println ("The file " + customersPath + " was not found.");
+			System.out.println ("The file was not found.");
 		}
 		catch (IOException exception)
 		{
-		 System.out.println (exception);
+			System.out.println (exception);
 		}
 		finally{
 			try{
@@ -32,10 +91,7 @@ public class CSVReader {
 				exception.printStackTrace();
 			}
 		}
-		
-		
 
-
+		System.out.println(distanceList);
 	}
-	
 }
---
+++ GetFatmaUber/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// creating objects
+		for (int i = 0; i < customersIDList.size(); i++) {
+			Customer customer + i = new Customer();
+			customer.se
+		}
+		
+	}
+
+}
